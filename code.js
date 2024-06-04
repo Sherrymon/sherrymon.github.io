d0065aca694f3dedf925309cb53f3f93c721e47d
Sherry <167395508+Sherrymon@users.noreply.github.com>
2024-06-04 06:22:06 +0200

    Delete code.js

--- code.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var path = window.location.pathname;
-    if (path.endsWith(".html")) {
-        var newPath = "fakediscord";
-        window.history.replaceState(null, "", newPath);
-    }
-});
